711b0346b6391c90a47196936a689e7459c9f78a
Kelarev Michail <60853520+Fourroubles@users.noreply.github.com>
2021-11-10 06:00:08 +0300

    Add files via upload

---
+++ ShoppingList.sol
@@ -0,0 +1,73 @@
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+pragma AbiHeader pubkey;
+
+import "DataAndInter.sol";
+
+contract ShoppingList {
+
+    mapping(uint32 => Purchase) m_purchase;
+    uint256 m_ownerPubkey;
+    uint32 m_count;
+
+    modifier onlyOwner() {
+        require(msg.pubkey() == m_ownerPubkey, 101);
+        _;
+    }
+
+    constructor( uint256 pubkey) public {
+        require(pubkey != 0, 120);
+        tvm.accept();
+        m_ownerPubkey = pubkey;
+    }
+
+    function getPurchases() public view returns (Purchase[] purchases) {
+        Purchase tmp;
+
+        for((uint32 id, Purchase purchase) : m_purchase) {
+            tmp.name = purchase.name;
+            tmp.amount = purchase.amount;
+            tmp.timeCreate = purchase.timeCreate;
+            tmp.isBuy = purchase.isBuy;
+            tmp.price = purchase.price;
+            purchases.push(Purchase(id, tmp.name, tmp.amount, tmp.timeCreate, tmp.isBuy, tmp.price));
+        }
+    }
+
+    function addPurchase(string name, uint amount) public onlyOwner {
+        tvm.accept();
+        m_count++;
+        m_purchase[m_count] = Purchase(m_count, name, amount, now, false, 0);
+    }
+
+    function deletePurchase(uint32 id) public onlyOwner {
+        require(m_purchase.exists(id), 102);
+        tvm.accept();
+        delete m_purchase[id];
+    }
+
+    function buy(uint32 id, uint32 price) public onlyOwner {
+        require(m_purchase.exists(id), 102);
+        tvm.accept();
+        m_purchase[id].isBuy = true;
+        m_purchase[id].price = price;
+    }
+
+    function getSummaryPurchase() public view returns (SummaryPurchase summaryPurchase) {
+        uint32 paidCount;
+        uint32 unPaidCount;
+        uint32 totalPrice;
+
+        for((, Purchase purchase) : m_purchase) {
+            if  (purchase.isBuy) {
+                unPaidCount++;
+            } 
+            else {
+                paidCount++;
+                totalPrice += purchase.price;
+            }
+        }
+
+        summaryPurchase = SummaryPurchase(paidCount, unPaidCount, totalPrice);
+    }
+}
